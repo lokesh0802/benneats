2554849de429f9f41c483d6c95cf0464b97d59ff
Lokesh kumar <121431788+lokesh0802@users.noreply.github.com>
2024-02-03 22:06:17 +0530

    first commit

---
+++ js/firstpage.js
@@ -0,0 +1,11 @@
+const buttonuser = document.getElementById("Userlogin");
+
+buttonuser.addEventListener("click", () => {
+  window.location.href = "login.html"; // Replace with the actual path to your HTML file
+});
+
+const buttonadmin = document.getElementById("Adminlogin");
+
+buttonadmin.addEventListener("click", () => {
+  window.location.href = "login.html"; // Replace with the actual path to your HTML file
+});
---
+++ js/form.js
@@ -0,0 +1,93 @@
+// form loading animation
+
+const form = [...document.querySelector('.form').children];
+
+form.forEach((item, i) => {
+    setTimeout(() => {
+        item.style.opacity = 1;
+    }, i*100);
+})
+
+window.onload = () => {
+    if(sessionStorage.name){
+        location.href = '/';
+    }
+}
+
+// form validation
+
+const name = document.querySelector('.name') || null;
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const submitBtn = document.querySelector('.submit-btn');
+
+if(name == null){ // means login page is open
+    submitBtn.addEventListener('click', () => {
+        fetch('/login-user',{
+            method: 'post',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({
+                email: email.value,
+                password: password.value
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            validateData(data);
+        })
+    })
+} else{ // means register page is open
+
+    submitBtn.addEventListener('click', () => {
+        fetch('/register-user', {
+            method: 'post',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({
+                name: name.value,
+                email: email.value,
+                password: password.value
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            validateData(data);
+        })
+    })
+
+}
+
+const validateData = (data) => {
+    if(!data.name){
+        alertBox(data);
+    } else{
+        sessionStorage.name = data.name;
+        sessionStorage.email = data.email;
+        location.href = '/';
+    }
+}
+
+const alertBox = (data) => {
+    const alertContainer = document.querySelector('.alert-box');
+    const alertMsg = document.querySelector('.alert');
+    alertMsg.innerHTML = data;
+
+    alertContainer.style.top = `5%`;
+    setTimeout(() => {
+        alertContainer.style.top = null;
+    }, 5000);
+}
+
+// if i want to open my tab on new page write this code other wise write next code given bellow 
+// const button = document.getElementById("myloginbutton");
+
+// button.addEventListener("click", () => {
+//   window.open("admin.html"); // Replace with the actual path to your HTML file
+// });
+
+
+const button = document.getElementById("myloginbutton");
+
+button.addEventListener("click", () => {
+  window.location.href = "adminpage.html"; // Replace with the actual path to your HTML file
+});
+
